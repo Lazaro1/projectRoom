40d8162efbe2327b28efb385627dfb60fb811144
Lazaro1 <lazarolimasilva2@gmail.com>
2022-08-08 16:52:46 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function processar(array, func) {
+  let resultado = [];
+
+  for (let i = 0; i < array.length; i++) {
+    resultado.push(func(array[i]));
+  }
+
+  return resultado;
+}
+
+function adicionarHttp(url) {
+  return "http://" + url;
+}
+
+function adicionarHttp(url) {
+  return; // escreva seu código aqui
+}
+
+function processar(__, __) {
+  // escreva seu código aqui
+}
